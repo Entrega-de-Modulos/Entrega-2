fb7e98abeb099edd6fc3468ecbb011ab5d28e598
tamirisrc <tamiris.carvalho01@fatec.sp.gov.br>
2025-11-18 23:14:17 -0300

    feat: setup do gateway

--- auth-service/api/src/main/java/com/fatec/security/AuthService.java
+++ auth-service/api/src/main/java/com/fatec/security/AuthService.java
@@ -4,9 +4,12 @@ import com.fatec.entities.Role;
 import com.fatec.entities.User;
 import com.fatec.exceptions.DuplicateEmailException;
 import com.fatec.repository.UserRepository;
+import com.fatec.security.dtos.AuthUserDetails;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -17,10 +20,11 @@ import java.time.Instant;
 import java.time.Period;
 import java.time.temporal.TemporalAmount;
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class AuthService {
-    @Value("jwt.secret")
+    @Value("${jwt.secret}")
     private String jwtSecret;
 
     private final UserRepository userRepository;
@@ -34,12 +38,19 @@ public class AuthService {
     }
 
 
-    private String generateToken(String email) {
+    private String generateToken(String email, List<Role> roles) {
         var issuedAt = Instant.now();
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(Date.from(issuedAt))
                 .expiration(Date.from(issuedAt.plus(Duration.ofHours(1))))
+                .claim("roles", roles)
+                .signWith(
+                    Keys.hmacShaKeyFor(
+                            jwtSecret.getBytes()
+                    ),
+                    Jwts.SIG.HS256
+                )
                 .compact();
     }
 
@@ -49,12 +60,15 @@ public class AuthService {
         }
         var user = new User(null, email, passwordEncoder.encode(password), Role.USER);
         userRepository.save(user);
-        return generateToken(email);
+        return generateToken(email, List.of(Role.USER));
     }
 
     public String login(String email, String password) {
         var authToken = new UsernamePasswordAuthenticationToken(email, password);
-        authenticationManager.authenticate(authToken);
-        return generateToken(email);
+        Authentication authentication = authenticationManager.authenticate(authToken);
+        var userDetails = (AuthUserDetails) authentication.getPrincipal();
+        return generateToken(userDetails.getUsername(), userDetails.getAuthorities().stream()
+                .map(grantedAuthority -> Role.valueOf(grantedAuthority.getAuthority().replace("ROLE_", "")))
+                .toList());
     }
 }
